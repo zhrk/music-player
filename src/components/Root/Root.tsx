caa22f88bfa2c122395d6f71906c9b73496d144d
zhrk <zshchipets@gmail.com>
2025-07-13 02:02:02 +0400

    cleaning

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,28 +1,9 @@
-import { useEffect } from 'react';
 import { Player } from '../Player';
 import styles from './styles.module.scss';
 
-export const Root = () => {
-  useEffect(() => {
-    const initCSSVars = async () => {
-      let vars = await window.electron.initCSSVars();
-
-      vars = Object.entries(vars)
-        .map(([key, value]) => `${key}: ${value}`)
-        .join('; ');
-
-      const style = document.createElement('style');
-      style.textContent = `:root { ${vars} }`;
-      document.head.appendChild(style);
-    };
-
-    initCSSVars();
-  }, []);
-
-  return (
-    <>
-      <div className={styles.titleBar} />
-      <Player />
-    </>
-  );
-};
+export const Root = () => (
+  <>
+    <div className={styles.titleBar} />
+    <Player />
+  </>
+);
