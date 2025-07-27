a4ebf1ddc64848ec62483169e7d9896098064c64
zhrk <zshchipets@gmail.com>
2025-07-28 02:29:46 +0400

    fix volume change rerenders

--- src/stores/player.ts
+++ src/stores/player.ts
@@ -1,4 +1,5 @@
 import { createWithEqualityFn as create } from 'zustand/traditional';
+import { shallow } from 'zustand/vanilla/shallow';
 
 type Playing = boolean;
 type Src = string | null;
@@ -21,21 +22,24 @@ interface State {
   setTotal: (payload: Progress) => void;
 }
 
-export const usePlayerStore = create<State>()((set, get) => ({
-  element: null,
-  src: null,
-  playing: false,
-  volume: 0.01,
-  progress: 0,
-  total: 0,
-  setElement: (payload) => {
-    if (payload) payload.volume = get().volume;
+export const usePlayerStore = create<State>()(
+  (set, get) => ({
+    element: null,
+    src: null,
+    playing: false,
+    volume: 0.01,
+    progress: 0,
+    total: 0,
+    setElement: (payload) => {
+      if (payload) payload.volume = get().volume;
 
-    set({ element: payload });
-  },
-  setPlaying: (payload) => set({ playing: payload }),
-  setSrc: (payload) => set({ src: payload }),
-  setVolume: (payload) => set({ volume: payload }),
-  setProgress: (payload) => set({ progress: payload }),
-  setTotal: (payload) => set({ total: payload }),
-}));
+      set({ element: payload });
+    },
+    setPlaying: (payload) => set({ playing: payload }),
+    setSrc: (payload) => set({ src: payload }),
+    setVolume: (payload) => set({ volume: payload }),
+    setProgress: (payload) => set({ progress: payload }),
+    setTotal: (payload) => set({ total: payload }),
+  }),
+  shallow
+);
