422f2dbfb5c36e4ee3a55aaa4442bcb4999d9c3a
zhrk <zshchipets@gmail.com>
2025-09-08 01:50:22 +0400

    fix drag

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -2,18 +2,15 @@ import styles from './styles.module.scss';
 
 export const TitleBar = () => (
   <div className={styles.container}>
-    <div className={styles.drag} />
-    <div className={styles.controls}>
-      <button type="button" className={styles.minimize}>
-        <div />
-      </button>
-      <button type="button" className={styles.maximize}>
-        <div />
-      </button>
-      <button type="button" className={styles.close}>
-        <div />
-        <div />
-      </button>
-    </div>
+    <button type="button" className={styles.minimize}>
+      <div />
+    </button>
+    <button type="button" className={styles.maximize}>
+      <div />
+    </button>
+    <button type="button" className={styles.close}>
+      <div />
+      <div />
+    </button>
   </div>
 );
