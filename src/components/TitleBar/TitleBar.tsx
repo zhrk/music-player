091545dc41ca538558685f77b9aa0e4f7b877f91
zhrk <zshchipets@gmail.com>
2025-09-08 01:46:39 +0400

    added title bar control buttons

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -1,3 +1,19 @@
 import styles from './styles.module.scss';
 
-export const TitleBar = () => <div className={styles.container} />;
+export const TitleBar = () => (
+  <div className={styles.container}>
+    <div className={styles.drag} />
+    <div className={styles.controls}>
+      <button type="button" className={styles.minimize}>
+        <div />
+      </button>
+      <button type="button" className={styles.maximize}>
+        <div />
+      </button>
+      <button type="button" className={styles.close}>
+        <div />
+        <div />
+      </button>
+    </div>
+  </div>
+);
