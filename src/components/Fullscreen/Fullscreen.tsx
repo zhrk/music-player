6c321226c0da0dc33fdb55abaa6c00ec8ccb50f8
zhrk <zshchipets@gmail.com>
2025-09-10 22:55:33 +0400

    cover maximize

--- src/components/Fullscreen/Fullscreen.tsx
+++ src/components/Fullscreen/Fullscreen.tsx
@@ -1,4 +1,6 @@
+import clsx from 'clsx';
 import { useState } from 'react';
+import IconMaximize from '../../static/icons/maximize.svg';
 import { usePlayerStore } from '../../stores/player';
 import { getAverageColor } from '../../utils/getAverageColor';
 import styles from './styles.module.scss';
@@ -6,13 +8,20 @@ import styles from './styles.module.scss';
 export const Fullscreen = () => {
   const { src } = usePlayerStore((state) => ({ src: state.src }));
 
+  const [maximized, setMaximized] = useState(false);
   const [color, setColor] = useState<string | null>(null);
 
   if (!src) return null;
 
   return (
-    <div className={styles.container} {...(color && { style: { backgroundColor: color } })}>
-      <div>
+    <div
+      className={clsx(styles.container, maximized && styles.maximized)}
+      {...(color && { style: { backgroundColor: color } })}
+    >
+      <div className={styles.cover}>
+        <button type="button" onClick={() => setMaximized((prev) => !prev)}>
+          <IconMaximize />
+        </button>
         <img
           alt=""
           src={`http://localhost:4445/cover/${encodeURIComponent(src)}`}
